76496f671d22bdb7119f0473a63fabf7d44f349c
Maty Hattam <maty.hattam@gmail.com>
2022-06-24 00:41:30 +0200

    finished app

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blueGrey[900],
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 60,
@@ -35,54 +36,46 @@ class MyApp extends StatelessWidget {
                   fontFamily: 'SourceSansPro',
                   fontSize: 20.0,
                   letterSpacing: 2.5,
-                  color: Colors.teal[100],
+                  color: Colors.blueGrey[100],
                 ),
               ),
-              Container(
-                padding: EdgeInsets.all(10),
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                color: Colors.white,
-                child: Row(
-                  children: <Widget>[
-                    Icon(
+              SizedBox(
+                height: 20,
+                child: Divider(color: Colors.blueGrey[100]),
+                width: 150,
+              ),
+              Card(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                  color: Colors.white,
+                  child: ListTile(
+                    leading: Icon(
                       Icons.phone,
-                      color: Colors.teal,
+                      color: Colors.blueGrey,
                     ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
+                    title: Text(
                       '+33 6 61 32 88 74',
                       style: TextStyle(
-                          color: Colors.teal,
+                          color: Colors.blueGrey,
                           fontFamily: 'SourceSansPro',
                           fontSize: 15),
                     ),
-                  ],
-                ),
-              ),
-              Container(
+                  )),
+              Card(
                 margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                padding: EdgeInsets.all(10),
                 color: Colors.white,
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.email,
-                      color: Colors.teal,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.blueGrey,
+                  ),
+                  title: Text(
+                    'maty.hattam.pro@gmail.com',
+                    style: TextStyle(
+                      color: Colors.blueGrey,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 15,
                     ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      'maty.hattam.pro@gmail.com',
-                      style: TextStyle(
-                        color: Colors.teal,
-                        fontFamily: 'SourceSansPro',
-                        fontSize: 15,
-                      ),
-                    )
-                  ],
+                  ),
                 ),
               )
             ],
